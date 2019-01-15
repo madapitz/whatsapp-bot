08f6661132c9c4883537f55186c8e821a00b8059
Manuel <manuelsapology@gmail.com>
2019-01-15 13:23:32 -0400

    cambio

---
+++ layer2.py
@@ -0,0 +1,39 @@
+from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
+from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
+from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
+from yowsup.layers.protocol_acks.protocolentities      import OutgoingAckProtocolEntity
+
+
+
+class EchoLayer(YowInterfaceLayer):
+
+    @ProtocolEntityCallback("message")
+    def onMessage(self, messageProtocolEntity):
+        #send receipt otherwise we keep receiving the same message over and over
+        
+        if True:
+
+        	#notificacion de leido
+            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
+            
+            #mensaje de respuesta
+
+            # if messageProtocolEntity.getBody() == 'furries':
+            #     outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+            #     'Los furries son un maldito cancer',
+            #     to = messageProtocolEntity.getFrom())
+            # else:
+            #     outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+            #     'Hablame qlq',
+            #     to = messageProtocolEntity.getFrom())
+
+            
+            
+            # print(messageProtocolEntity)
+            self.toLower(receipt)
+            # self.toLower(outgoingMessageProtocolEntity)
+    
+    @ProtocolEntityCallback("receipt")
+    def onReceipt(self, entity):
+        ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", entity.getType(), entity.getFrom())
+        self.toLower(ack)
